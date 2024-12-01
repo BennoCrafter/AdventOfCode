1cd25f07c541a6577d15e6a4f4a24de44720f204
Benno <107504783+BennoCrafter@users.noreply.github.com>
2024-12-01 10:56:54 +0100

    Update puzzle.py
    Refactor

--- automation/puzzle.py
+++ automation/puzzle.py
@@ -6,6 +6,16 @@ from automation.fetch_input import fetch_input
 from automation.utils.read_file import read_file
 from automation.utils.write_file import write_file
 
+def create_input_file(year: int, day: int, inputs_dir_path: Path, puzzle_name: str) -> bool:
+    try:
+        input_data = fetch_input(year, day)
+    except Exception as e:
+        click.echo(click.style(f"Error fetching input: {str(e)}", fg="red"))
+        return False
+
+    # Create input.txt file
+    write_file(inputs_dir_path / f"input_{puzzle_name}.txt", input_data)
+    return True
 
 def open_puzzle(url: str):
     """Open the Advent of Code puzzle in Safari."""
@@ -27,6 +37,9 @@ def create_puzzle_template(input_url: str, year: int, day: int, is_forced: bool)
 
     puzzle_dir_path = solutions_dir_path / puzzle_name
 
+    if not create_input_file(year, day, inputs_dir_path, puzzle_name):
+        return
+
     # Handle forced overwriting
     if puzzle_dir_path.exists():
         if is_forced:
@@ -36,22 +49,7 @@ def create_puzzle_template(input_url: str, year: int, day: int, is_forced: bool)
             click.echo(click.style(f"The directory {puzzle_dir_path} already exists. Use '--force' to overwrite.", fg="yellow"))
             return
 
-    # Create a new puzzle directory
     puzzle_dir_path.mkdir(parents=True)
-
-    # Fetch input file from the provided URL
-    if not input_url:
-        click.echo(click.style("No input URL provided.", fg="red"))
-        return
-
-    try:
-        input_data = fetch_input(year, day)  # Fetching input using the refactored function
-    except Exception as e:
-        click.echo(click.style(f"Error fetching input: {str(e)}", fg="red"))
-        return
-
-    # Create input.txt file
-    write_file(inputs_dir_path / f"input_{puzzle_name}.txt", input_data)
 
     # Create part_1.py and part_2.py files
     for part in range(1, 3):
