87a21c04047a9eda803b30bd0b47684314c49b66
Benno <107504783+BennoCrafter@users.noreply.github.com>
2024-12-05 22:09:53 +0100

    Added solution for day 5 of year 2024

---
+++ years/2024/solutions/05/part_1.py
@@ -0,0 +1,49 @@
+from typing import Any
+from automation.get_input import get_input
+
+input: str = get_input(2024, 5)
+
+
+class Rule:
+    def __init__(self, num: str, not_exceed: list[str]):
+        self.num = num
+        self.not_exceed = not_exceed
+
+def get_rule(rules: list[Rule], num: str) -> Rule | None:
+    for rule in rules:
+        if rule.num == num:
+            return rule
+    return None
+
+
+def main() -> Any:
+    page_ordering_rules, updates = input.split("\n\n")
+    rules: list[Rule] = []
+    valid_updates: list[list[str]] = []
+
+    for por in page_ordering_rules.split("\n"):
+        num, not_exceed = por.split("|")
+
+        fr = get_rule(rules, num)
+        if fr == None:
+            rules.append(Rule(num, [not_exceed]))
+        else:
+            fr.not_exceed.append(not_exceed)
+
+    for update in updates.split("\n"):
+        nums: list[str] = update.split(",")
+        is_valid = True
+
+        for i, num in enumerate(nums):
+            rule = get_rule(rules, num)
+            if rule == None:
+                continue
+
+            if any(item in nums[:i] for item in rule.not_exceed):
+                is_valid = False
+                break
+
+        if is_valid:
+            valid_updates.append(nums)
+
+    return sum(int(vu[len(vu)//2]) for vu in valid_updates)
---
+++ years/2024/solutions/05/part_2.py
@@ -0,0 +1,7 @@
+from typing import Any
+from automation.get_input import get_input
+
+input: str = get_input(2024, 5)
+
+def main() -> Any:
+    return
